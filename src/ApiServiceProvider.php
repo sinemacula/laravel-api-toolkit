b88aec9265d2d7726218cc3657b1792b5aaff090
Ben Carey <bdmc@sinemacula.co.uk>
2024-04-16 13:18:27 +0100

    Initial foundation commit

---
+++ src/ApiServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SineMacula;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * API service provider.
+ *
+ * @author      Ben Carey <bdmc@sinemacula.co.uk>
+ * @copyright   2024 Sine Macula Limited.
+ */
+class ApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
