a18cc66f5adfaf0c1297b6a126bd1c77f6497336
Ben Carey <sinemacula-ben@users.noreply.github.com>
2024-04-24 10:40:26 +0000

    Apply fixes from StyleCI

--- src/Http/Middleware/JsonPrettyPrint.php
+++ src/Http/Middleware/JsonPrettyPrint.php
@@ -18,7 +18,7 @@ class JsonPrettyPrint
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure                  $next
+     * @param  \Closure  $next
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
