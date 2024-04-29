235749fa752e917afe9d243edcd1d0dedba3f012
Ben Carey <sinemacula-ben@users.noreply.github.com>
2024-04-29 09:58:02 +0000

    Apply fixes from StyleCI

--- src/Exceptions/ApiExceptionType.php
+++ src/Exceptions/ApiExceptionType.php
@@ -17,43 +17,43 @@ class ApiExceptionType
     | General Errors
     |----------------------------------------------------------------------------
     */
-    const array GENERAL_ERROR = ['code' => 10001, 'status' => 500];
+    public const array GENERAL_ERROR = ['code' => 10001, 'status' => 500];
 
     /*
     |----------------------------------------------------------------------------
     | HTTP Errors
     |----------------------------------------------------------------------------
     */
-    const array BAD_REQUEST       = ['code' => 10100, 'status' => 400];
-    const array UNAUTHORIZED      = ['code' => 10101, 'status' => 401];
-    const array FORBIDDEN         = ['code' => 10102, 'status' => 403];
-    const array NOT_FOUND         = ['code' => 10103, 'status' => 404];
-    const array NOT_ALLOWED       = ['code' => 10104, 'status' => 405];
-    const array INVALID_INPUT     = ['code' => 10105, 'status' => 422];
-    const array TOO_MANY_ATTEMPTS = ['code' => 10106, 'status' => 429];
+    public const array BAD_REQUEST       = ['code' => 10100, 'status' => 400];
+    public const array UNAUTHORIZED      = ['code' => 10101, 'status' => 401];
+    public const array FORBIDDEN         = ['code' => 10102, 'status' => 403];
+    public const array NOT_FOUND         = ['code' => 10103, 'status' => 404];
+    public const array NOT_ALLOWED       = ['code' => 10104, 'status' => 405];
+    public const array INVALID_INPUT     = ['code' => 10105, 'status' => 422];
+    public const array TOO_MANY_ATTEMPTS = ['code' => 10106, 'status' => 429];
 
     /*
     |----------------------------------------------------------------------------
     | API Errors
     |----------------------------------------------------------------------------
     */
-    const array MAINTENANCE_MODE    = ['code' => 10200, 'status' => 503];
-    const array MODEL_NOT_PARSEABLE = ['code' => 10201, 'status' => 500];
+    public const array MAINTENANCE_MODE    = ['code' => 10200, 'status' => 503];
+    public const array MODEL_NOT_PARSEABLE = ['code' => 10201, 'status' => 500];
 
     /*
     |----------------------------------------------------------------------------
     | File Errors
     |----------------------------------------------------------------------------
     */
-    const array FILE_UPLOAD_ERROR = ['code' => 10300, 'status' => 500];
-    const array INVALID_IMAGE     = ['code' => 10301, 'status' => 422];
+    public const array FILE_UPLOAD_ERROR = ['code' => 10300, 'status' => 500];
+    public const array INVALID_IMAGE     = ['code' => 10301, 'status' => 422];
 
     /*
     |----------------------------------------------------------------------------
     | Notification Errors
     |----------------------------------------------------------------------------
     */
-    const array INVALID_NOTIFICATION = ['code' => 10400, 'status' => 500];
-    const array FAILED_TO_SEND_SMS   = ['code' => 10401, 'status' => 500];
+    public const array INVALID_NOTIFICATION = ['code' => 10400, 'status' => 500];
+    public const array FAILED_TO_SEND_SMS   = ['code' => 10401, 'status' => 500];
 
 }
