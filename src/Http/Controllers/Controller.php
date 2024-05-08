cae89661ee6d230d7f2d36dd7a8be4117904fe23
Ben Carey <bdmc@sinemacula.co.uk>
2024-05-08 20:56:12 +0100

    Added API controller

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace SineMacula\ApiToolkit\Http\Controllers;
+
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Routing\Controller as LaravelController;
+use Illuminate\Support\Facades\Response;
+
+/**
+ * Base API controller.
+ *
+ * @author      Ben Carey <ben.carey@verifast.com>
+ * @copyright   2024 Verifast, Inc.
+ */
+abstract class Controller extends LaravelController
+{
+    use ValidatesRequests;
+
+    /**
+     * Respond with raw array data as a JSON response.
+     *
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithData(array $data, int $status = 200, array $headers = []): JsonResponse
+    {
+        return Response::json(['data' => $data], $status, $headers);
+    }
+
+    /**
+     * Respond with a JSON resource representing a single item.
+     *
+     * @param  \Illuminate\Http\Resources\Json\JsonResource  $resource
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithItem(JsonResource $resource, int $status = 200, array $headers = []): JsonResponse
+    {
+        return $resource->response()->setStatusCode($status)->withHeaders($headers);
+    }
+
+    /**
+     * Respond with a JSON resource collection.
+     *
+     * @param  \Illuminate\Http\Resources\Json\ResourceCollection  $collection
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithCollection(ResourceCollection $collection, int $status = 200, array $headers = []): JsonResponse
+    {
+        return $collection->response()->setStatusCode($status)->withHeaders($headers);
+    }
+}
