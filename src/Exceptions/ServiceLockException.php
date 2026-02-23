b416f35adf467fb8d68e357450643d846ee485fc
Ben Carey <bdmc@sinemacula.co.uk>
2026-02-22 21:35:06 -0500

    Removed crap Codex tests

---
+++ src/Exceptions/ServiceLockException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SineMacula\ApiToolkit\Exceptions;
+
+/**
+ * Exception thrown when a service lock cannot be established.
+ *
+ * @author      Ben Carey <bdmc@sinemacula.co.uk>
+ * @copyright   2026 Sine Macula Limited.
+ */
+final class ServiceLockException extends \RuntimeException {}
